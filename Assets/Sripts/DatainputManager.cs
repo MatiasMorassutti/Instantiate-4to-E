102bedff901fa4db42020dd70e3834ef1759afb7
MatiasMorassutti <morassuttimatias@gmail.com>
2022-05-10 15:27:12 -0300

    Input field for text

---
+++ Assets/Sripts/DatainputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DatainputManager : MonoBehaviour
+{
+    public Text txtUseName;
+
+   public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUseName.text);
+    }
+}
